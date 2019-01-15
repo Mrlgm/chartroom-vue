8515e2791e8079d0adc7e5071489b73ebf3b8f3c
Mrlgm <877205619@qq.com>
2019-01-15 22:52:34 +0800

    添加路由守卫

--- src/router.js
+++ src/router.js
@@ -1,20 +1,46 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import store from './store'
 
 Vue.use(Router)
-
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/conversations',
-      name: 'conversations',
-      component: () => import(/* webpackChunkName: "about" */ './views/Conversations.vue')
-    }
-  ]
+const router = new Router({
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/conversations',
+            name: 'conversations',
+            meta: {
+                requireLogin: true
+            },
+            component: () => import(/* webpackChunkName: "about" */ './views/Conversations.vue'),
+            children: [
+                {
+                    // 当 /user/:id/profile 匹配成功，
+                    // UserProfile 会被渲染在 User 的 <router-view> 中
+                    path: '/:id',
+                    component: () => import(/* webpackChunkName: "about" */ './components/MessageContent'),
+                }
+            ]
+        }
+    ]
 })
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requireLogin)) {
+        if (!store.state.isLogin) {
+            next('/')
+        } else {
+            next()
+        }
+    } else {
+        next()
+    }
+})
+
+export default router
+
